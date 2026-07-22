e715deac19f4bdaa002d8a296bcfe2d1a160dd97
ninjanights <abirdasart@gmail.com>
2026-07-22 23:44:38 +0530

    Remove local embedding model

--- core/model_registry.py
+++ core/model_registry.py
@@ -1,17 +1,8 @@
-from sentence_transformers import SentenceTransformer
 from rag_engine.llm.llm_itself import LLMItself
 
 
 class ModelRegistry:
-    _embedding_model = None
     _llm_model = None
-
-    @classmethod
-    def embedding_model(cls):
-        if cls._embedding_model is None:
-            cls._embedding_model = SentenceTransformer("BAAI/bge-small-en-v1.5")
-
-        return cls._embedding_model
 
     @classmethod
     def llm_model(cls):
